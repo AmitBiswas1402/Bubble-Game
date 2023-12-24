016d1bbf62b3778c887621ef99f2aa8d8f96d7f5
Amit Biswas <115923314+AmitBiswas1402@users.noreply.github.com>
2023-12-24 08:45:35 +0000

    bubble game initial push

---
+++ Bubble game/bubble.js
@@ -0,0 +1,49 @@
+let timer = 5;
+let score = 0;
+let random = 0;
+
+function incScore(){
+    score += 10;
+    document.querySelector("#score-val").textContent = score;
+}
+
+function getNewHit(){
+    random = Math.floor(Math.random()*10);
+    document.querySelector("#hit-value").textContent = random;
+}
+
+function makeBubble(){
+    let clutter="";
+
+    for (let i=0; i<=74; i++){
+        let random = Math.floor(Math.random()*10)
+        clutter += `<div class="bubble">${random}</div>`;
+    }
+    document.querySelector(".panel-bottom").innerHTML = clutter;
+}
+
+function runTimer(){
+    let timerint = setInterval(function(){
+        if(timer>0){
+            timer--;
+            document.querySelector("#timerval").textContent=timer;
+        } else{
+            clearInterval(timerint);
+            document.querySelector(".panel-bottom").innerHTML = `<h1>Game Over</h1>`;
+        }
+    }, 1000);
+}
+
+document.querySelector(".panel-bottom").addEventListener("click", function(dets){
+    let clickNum = Number(dets.target.textContent);
+    if(clickNum === random){
+        incScore();
+        makeBubble();
+        getNewHit();
+    }
+})
+
+getNewHit();
+runTimer();
+makeBubble();
+incScore();
